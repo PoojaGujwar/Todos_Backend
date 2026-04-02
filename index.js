dbbbd25284056cf6fe1fae35ffeea1c9a636ee78
Pooja Gujwar <gujwarpooja@gmail.com>
2026-04-02 15:36:11 +0530

    Update git file

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const {initialDatabase} = require("./db/db.connection");
+require("dotenv").config()
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = 4001;
+const port = 4001 || process.env.PORT;
 const todoRoutes = require("./routes/todosRoutes.js")
 
 initialDatabase();
